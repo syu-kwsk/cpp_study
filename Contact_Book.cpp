12e0dbd000ff510657baa70cb594981a9ea512e2
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-02-20 16:12:04 +0900

    Update Contact_Book.cpp

--- Contact_Book.cpp
+++ Contact_Book.cpp
@@ -1,13 +1,31 @@
 #include<iostream>
 using namespace std;
 
-void store();
-void display();
-
-int main(){
-  store();
-  display();
+class contact{
+  //contactの非公開メンバ
+  string name;
+  string address;
+public:
+  //contactの公開関数
+  void store();
+  void display();
+};
 
   
+int main(){
+  contact person;
+  person.store();
+  person.display();
+  
   return 0;
+}
+
+void contact::store(){
+  cout << "name:";
+  cin >> name;
+  cout << "address:";
+  cin >> address;
+}
+void contact::display(){
+  cout << name << " lives " << address << "\n";
 }
