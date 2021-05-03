8ad1fe4daec5b9e77c57c67b2892fb9b417cc73b
syu-kwsk <wataru1165kwsk@gmail.com>
2021-05-03 18:57:45 +0900

    Add a_round_up_the_mean

---
+++ apg4b/practice/a_round_up_the_mean.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int a, b;
+  cin >> a >> b;
+
+  double ave = (a + b) / 2.0;
+  int ave_i = ave + 0.5;
+  cout << ave_i << endl;
+}
+
